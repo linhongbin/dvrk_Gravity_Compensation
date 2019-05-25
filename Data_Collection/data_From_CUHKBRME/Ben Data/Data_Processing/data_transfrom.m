f3a5a2af2289d532459ef14e55db87438656f951
Ben Lin <522181941@qq.com>
2019-05-25 21:33:55 +0800

    commit symbolic file for paper

--- Data_Collection/data_From_CUHKBRME/Ben Data/Data_Processing/data_transfrom.m
+++
@@ -1,8 +0,0 @@
-d_size = size(torque_current);
-Torques_data = zeros(7,2,d_size(2)*d_size(3));
-for i=1:d_size(2)
-    for j=1:d_size(3)
-        Torques_data(:,1,i*20+j) = actual_position(:,i,j);
-        Torques_data(:,2,i*20+j) = torque_current(:,i,j);
-    end
-end
--- Data_Collection/data_From_CUHKBRME/Vincent Data/data_process/Torque_data_angles_torque_plot.m
+++
@@ -1,8 +0,0 @@
-for i = 1:7
-    figure(i)
-    angles = Torques_data(i,1,:);
-    torque = Torques_data(i,2,:);
-    scatter(angles,torque);
-    title_name = sprintf('Joint %d torques with angles',i)
-    title(title_name);
-end
